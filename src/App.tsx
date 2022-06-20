9e56d394266ba3553dfdf0c0a0c57b49f32ecccb
guraogr <guraogr@gmail.com>
2022-06-20 22:00:29 +0900

    fix: セミコロンの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import logo from './logo.svg'
-import './App.css'
+import logo from './logo.svg';
+import './App.css';
 
 const App = () => (
   <div className="App">
@@ -13,6 +13,6 @@ const App = () => (
       </a>
     </header>
   </div>
-)
+);
 
-export default App
+export default App;
